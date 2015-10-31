43df594c9eee23b571437cd3531234c4333a7875
zuzadutkiewicz <zuza.dutkiewicz@gmail.com>
2015-10-31 18:11:40 +0100

    Wczytywanie danych z pliku, zmiana na integer i wyświetlanie

--- main.cpp
+++ main.cpp
@@ -1,59 +1,53 @@
 #include <iostream>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <conio.h>
 #include <fstream>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
-
-char * nazwaPliku;
-int wierzcholek=0;
-
-void podajIodczytaj();
-void odczytajZPliku(char * nazwaPliku);
-
-
-
-void podajIodczytaj()
-{
-    char nazwa [30];
-    printf("Podaj nazwe pliku: ");
-    scanf("%s", nazwa);
-    odczytajZPliku(nazwa);
-}
-
-void odczytajZPliku(char * nazwaPliku)
-{
-    fstream plik;
-    string line;
-
-
-
-    plik.open(nazwaPliku, ios::in);
-    {
-        while(getline(plik,line))
-        {
-            const char * buf = line.c_str();
-            sscanf(buf, "%d", &wierzcholek);
-            printf("Linia: %s\n", buf);
-
-        }
-    }
-    plik.close();
-    printf("Odczyt z pliku zakonczony powodzeniem\n");
-}
-
-
+void odczytajZPliku(const char * nazwaPliku);
 
 int main()
 {
-  podajIodczytaj();
-  printf("Przed wierzochlkiem\n");
+    odczytajZPliku("test.csv");
+    return 0;
+}
 
-  printf("%d\n", wierzcholek);
-
-printf("po wierzochlkiem\n");
-
-  return 0;
+    void odczytajZPliku(const char * nazwaPliku)
+{
+    char buflicz[10];
+    fstream plik;
+    string line;
+    plik.open(nazwaPliku, ios::in);
+    {
+        int linia = 0;
+        int kolumna = 0;
+        while(getline(plik,line))
+        {
+            linia++;
+            const char * buf = line.c_str();
+            int i = 0;
+            int j = 0;
+            while(true) {
+                if(buf[i] != '\t' && buf[i] !='\0') {
+                    buflicz[j]=buf[i];
+                    j++;
+                } else {
+                    int dana = 0;
+                    kolumna++;
+                    buflicz[j] = '\0';
+                    dana = atoi(buflicz);
+                    // sscanf(buflicz, "%d", &dana);
+                    printf("linia: %d kolumna: %d liczba: %d\n", linia, kolumna, dana);
+                    j = 0;
+                }
+                if(buf[i] == '\0')
+                    break;
+            i++;
+            }
+            kolumna = 0;
+        }
+    }
+    plik.close();
+    printf("Odczyt z pliku zakonczony powodzeniem.\n");
 }
